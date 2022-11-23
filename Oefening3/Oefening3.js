49e0d501f5b473704970a0b3987bee93839bb0ad
Malik <87253@glr.nl>
2022-11-23 12:23:41 +0100

    save

--- Oefening3/Oefening3.js
+++ Oefening3/Oefening3.js
@@ -1,19 +1,39 @@
 let p = document.querySelector('p');
 let start = document.getElementById('start');
 let end = document.getElementById('end');
+let progress = document.getElementById('progress');
 let timer;
 
 function timerHandler() {
-    let number = Number(p.innerHTML) + 1;
+    let number = Number(p.innerHTML) - 1;
+
+    if (number == -1) {
+        number = 10;
+    }
+
+    if (number < 5) {
+        p.style.fontSize = "300px";
+        p.style.color = "red";
+        p.style.backgroundColor = "yellow";
+    }
+
+    if (number > 5) {
+        p.removeAttribute('style')
+    }
+
     p.innerHTML = number;
 }
+
 let checkerStart = 0;
 let checkerEnd = 0;
+
 start.addEventListener('click', () => {
     if (checkerStart < 1) {
         timer = setInterval(timerHandler, 1000);
         checkerStart = 1;
         checkerEnd = 0;
+        progress.style.animation = 'progress'; 
+        // hier werk je aan
     };
 });
 end.addEventListener('click', () => {
