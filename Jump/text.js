94387449a86553aea5665f400682780438b9b50e
Malik <87253@glr.nl>
2022-12-14 12:17:44 +0100

    save

--- Jump/text.js
+++ Jump/text.js
@@ -1,7 +1,15 @@
-setInterval(function tijd(){
-let time = new Date();
-let h = time.getHours();
-let m = time.getMinutes();
+setInterval(function tijd() {
+    let time = new Date();
+    let h = time.getHours();
+    let m = time.getMinutes();
 
-document.getElementById("kloktijd").innerHTML = h + ":" + m;
+    if (m < 10) {
+        m = "0" + m;
+    }
+
+    if (h < 10) {
+        h = "0" + h;
+    }
+
+    document.getElementById("kloktijd").innerHTML = h + ":" + m;
 }, 1000);
