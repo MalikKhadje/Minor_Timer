d4a0d5750be5e33e82900b4ce7130c13c259847e
Malik <87253@glr.nl>
2022-12-23 10:57:50 +0100

    save

--- test/test.js
+++ test/test.js
@@ -88,7 +88,7 @@ const currentTime = new Date();
 const currentHour = currentTime.getHours();
 const currentMinute = currentTime.getMinutes();
 
-if (currentHour < 12 || (currentHour === 12 && currentMinute === 0) || (currentHour > 18 || (currentHour === 18 && currentMinute === 0))) {
+if (currentHour < 10 || (currentHour === 12 && currentMinute === 0) || (currentHour > 18 || (currentHour === 18 && currentMinute === 0))) {
   document.getElementById("popup").style.display = "block";
 } else {
   document.getElementById("popup").style.display = "none"; 
